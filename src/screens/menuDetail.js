2cbcafcf1681ccca6cea69910b66ae179c12e040
MonikaAnkoliya <monika.ankoliya@gmail.com>
2020-12-06 18:05:40 +0100

    add loader while loading webview

--- src/screens/menuDetail.js
+++ src/screens/menuDetail.js
@@ -1,31 +1,59 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, SafeAreaView, View, ActivityIndicator} from 'react-native';
 import { useRoute } from "@react-navigation/native";
 import { WebView } from 'react-native-webview';
 
 function MenuDetail() {
   const route = useRoute();
+  const [visible, setVisible] = useState(false);
+
+  const ActivityIndicatorElement = () => {
+    return (
+      <View style={styles.activityIndicatorStyle}>
+        <ActivityIndicator color="#009688" size="large" />
+      </View>
+    );
+  };
+
   const uri = route.params.uri;
     return (
-      <WebView
-        ref={(ref) => { this.webview = ref; }}
-        source={{ uri }}
-      />
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <WebView
+          style={{ flex: 1 }}
+          //Loading URL
+          source={{ uri }}
+          //Enable Javascript support
+          javaScriptEnabled={true}
+          //For the Cache
+          domStorageEnabled={true}
+          onLoadStart={() => setVisible(true)}
+          onLoad={() => setVisible(false)}
+        />
+        {visible ? <ActivityIndicatorElement /> : null}
+      </View>
+    </SafeAreaView>
     )
   }
   
   const styles = StyleSheet.create({
     container: {
+      backgroundColor: '#F5FCFF',
       flex: 1,
-      backgroundColor: '#333',
-      justifyContent: 'center',
-      alignItems: 'center'
     },
-    boldText: {
-      color: '#fff',
-      fontSize: 24,
-      fontWeight: 'bold'
-    }
+    activityIndicatorStyle: {
+      flex: 1,
+      position: 'absolute',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      marginTop: 'auto',
+      marginBottom: 'auto',
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+      justifyContent: 'center',
+    },
   })
   
   export default MenuDetail
